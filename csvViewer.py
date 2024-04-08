af07075c970b6683482c8068ef10a75ed2411a97
Hicf25 <166264982+Hicf25@users.noreply.github.com>
2024-04-07 22:18:35 -0400

    Update csvViewer.py

--- csvViewer.py
+++ csvViewer.py
@@ -1,18 +1,14 @@
 import streamlit as st
 import pandas as pd
-from tkinter import messagebox
 
-try:
-    st.write(
-        '''# CSV file uploader/viewer
+st.write(
+    '''# CSV file uploader/viewer
 
-        '''
-    )
+    '''
+)
 
-    up_file = st.file_uploader(label='Upload a csv file', type=['csv'])
-    if up_file is not None:
-        data = pd.read_csv(up_file)
-        dataFrame = pd.DataFrame(data)
-        st.dataframe(dataFrame)
-except:
-    messagebox.showinfo('There was an error')
+up_file = st.file_uploader(label='Upload a csv file', type=['csv'])
+if up_file is not None:
+    data = pd.read_csv(up_file)
+    dataFrame = pd.DataFrame(data)
+    st.dataframe(dataFrame)
